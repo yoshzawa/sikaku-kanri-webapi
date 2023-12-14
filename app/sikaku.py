236dbd9d639150c6d63bdd17b81b3af47c0f822f
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-14 17:57:16 +0900

    Update sikaku.py

--- app/sikaku.py
+++ app/sikaku.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 # 商品リストのデータ
 products = [
-    {"ID": 10, "NAME": "APPLE", "PRICE": 250},
-    {"ID": 20, "NAME": "BANANA", "PRICE": 150}
+    {"ID": "FE00", "NAME": "基本情報技術者試験", "DATE": "2022/06/18"},
+    {"ID": "OR00", "NAME": "Java SE Bronze", "DATE": "2023/02/20"}
 ]
 
 @app.get("/list")
