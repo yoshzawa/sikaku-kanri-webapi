33f381114ea3fce846075205759fba3ca8fc23e1
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-14 18:00:50 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from sikaku import app as sikakuApp
+from voucher import app as voucherApp
 
 app = FastAPI()
 
 # "/list"にmain1.pyのアプリケーションをマウント
 app.mount("/sikaku", sikakuApp)
+app.mount("/voucher", voucherApp)
