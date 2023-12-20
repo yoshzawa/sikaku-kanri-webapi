0c7c369a02deb7a2a3b98cd41ec2f92ab0fdfb02
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-20 16:07:49 +0900

    Create exam.py

---
+++ app/exam.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# 資格リストのデータ
+Exams = [
+    {"ID": "FE00", "NAME": "基本情報技術者試験"},
+    {"ID": "OR00", "NAME": "Java SE Bronze"}
+]
+
+@app.get("/list")
+def get_exam_list():
+    return Exams
+
+@app.get("/{ID}")
+def get_exam_item(ID:str):
+    if ID == "FE00":
+        return Exams[0]
+    elif ID == "OR00":
+        return Exams[1]
+    else:
+        return {}
