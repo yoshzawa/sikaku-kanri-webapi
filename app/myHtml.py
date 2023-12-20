e19bfa37831e81ecd72781dff580e80d499712b1
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-20 17:52:59 +0900

    Update myHtml.py

--- app/myHtml.py
+++ app/myHtml.py
@@ -5,6 +5,8 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="html")  # テンプレートが存在するディレクトリを指定
 
+data = {"message": "Hello, World!"}  # テンプレートに渡すデータ
+
 @router.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("login.html", {"request": request, "data": data})
