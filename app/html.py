4a20026ce7ecf848a274590309a8ad6db0f54f49
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-20 17:13:17 +0900

    Update html.py

--- app/html.py
+++ app/html.py
@@ -1,9 +1,7 @@
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
 
-app = FastAPI()
 templates = Jinja2Templates(directory="html")  # テンプレートが存在するディレクトリを指定
 
-@app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("login.html", {"request": request, "data": data})
