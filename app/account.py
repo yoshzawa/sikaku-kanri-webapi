d0f93da4a4896ead9af0ca8c1b7d104c188b77a6
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-20 16:33:37 +0900

    Update account.py

--- app/account.py
+++ app/account.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.post("/login")
 def get_product_item(userID:str,passcd:str):
     if userID == "User1":
-        return {"Status":"OK",Token="Kakyouin131"}
+        return {"Status":"OK",Token":"Kakyouin131"}
     else:
         return {"Status":"ERROR","message":"Invalid username or incorrect password"}
 
