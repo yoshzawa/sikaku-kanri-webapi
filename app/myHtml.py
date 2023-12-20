83abc0d779db29c6eff0e189303825c29d39b50b
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-20 18:04:10 +0900

    Update myHtml.py

--- app/myHtml.py
+++ app/myHtml.py
@@ -1,13 +1,40 @@
-from fastapi import APIRouter
-from fastapi.requests import Request
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.templating import Jinja2Templates
+from typing import Optional, SecretStr
+import httpx
 
 router = APIRouter()
 templates = Jinja2Templates(directory="html")  # テンプレートが存在するディレクトリを指定
 
-data = {"message": "Hello, World!"}  # テンプレートに渡すデータ
 
 @router.get("/")
 async def read_root(request: Request):
+    data = {"message": "Hello, World!"}  # テンプレートに渡すデータ
     return templates.TemplateResponse("login.html", {"request": request, "data": data})
+
+
+async def login_user(username: str, password: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.post("/account/login", data={"username": username, "password": password})
+    
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Login failed")
+    
+    return response.json()
+
+@router.post("/check")
+async def check_user(request: Request, username: str, password: SecretStr):
+    login_data = await login_user(username, password)
+    
+    status = login_data.get("Status")
+    token = login_data.get("Token")
+    message = login_data.get("message")
+    
+    return {"Status": status, "Token": token, "message": message}
+
+
+
+
+
+
 
