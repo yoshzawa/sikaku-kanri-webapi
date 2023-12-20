79f0f514f0005cc1a874a302a61fdccd57dc51a1
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-20 19:34:05 +0900

    Update myHtml.py

--- app/myHtml.py
+++ app/myHtml.py
@@ -16,6 +16,8 @@ async def read_root(request: Request):
 async def login_user(base_url: str, username: str, password: str):
     login_url = "http://192.168.54.231:8888/account/login"
     print(f"Login URL: {login_url}")  # ログにURLを出力
+    print(f"Status Code: {response.status_code}")  # ステータスコードをログに出力
+
 
     async with httpx.AsyncClient() as client:
         response = await client.post(login_url, data={"username": username, "password": password})
