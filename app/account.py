d489cd5b9c440980212dd8e31856dc7c1d436462
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-20 16:03:40 +0900

    Create account.py

---
+++ app/account.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/login")
+def get_product_item(userID:str,passcd:str):
+    if userID == "User1":
+        return {"Status":"OK",Token="Kakyouin131"}
+    else:
+        return {"Status":"ERROR","message":"Invalid username or incorrect password"}
+
+@app.post("/checkToken")
+def get_product_item(userID:str,token:str):
+    if token == "Kakyouin131":
+        return {"Status":"OK"}
+    else:
+        return {"Status":"NG"}
