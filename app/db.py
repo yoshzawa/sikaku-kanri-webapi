2a7e2bbde326a95ea5fe200a5bfe4c1aa4727501
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-22 10:20:26 +0900

    Create db.py

---
+++ app/db.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# データベースの設定
+DB_USER = 'user1'
+DB_PASSWORD = 'password'
+DB_HOST = '192.168.54.100'
+DB_NAME = 'shop1'
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    item_id = Column(String, index=True)
+    item_name = Column(String, index=True)
+    price = Column(Integer)
+
+# テーブルが存在しない場合は作成する
+Base.metadata.create_all(bind=engine)
