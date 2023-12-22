2e7183221fb2d74add4cc6a2f6a335c7569f77a5
yoshzawa <yoshzawa@users.noreply.github.com>
2023-12-22 12:22:03 +0900

    Update db.py

--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 # データベースの設定
 DB_USER = 'user1'
 DB_PASSWORD = 'password'
-DB_HOST = '172.17.0.1'
+DB_HOST = '192.168.54.231'
 DB_NAME = 'shop1'
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
